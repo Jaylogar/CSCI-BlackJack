4f8aff55af65c485c545398cc67334221be418ad
Jaylogar <jaylogar2121@gmail.com>
2026-04-29 02:15:52 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 1. Declare four boolean variables: isRed, isYellow,
+    // isGreen, and policeStops
+
+    // 2. Prompt the user and get input for isRed,
+    // isYellow, and isGreen (1 for true, 0 for false)
+
+    // 3. Prompt the user and get input for policeStops
+
+    
+    // --- Task A: Nested Logic ---
+    // Check if the light is green
+    //    Inside the green check, check if the 
+    //    police are stopping the vehicle
+    //        If true, output "Stop"
+    //        If false (else), output "Go"
+    // Else if the light is yellow
+    //     Output "Get ready to move"
+    // Else if the light is red
+    //     Output "Stop"
+
+    
+    // visual break btw tasks
+    cout << "________________________________\n"; 
+
+    
+    // --- Task B: Logical Operators ---
+    // Use a single if-else statement to check if:
+    // light is green AND the police does not stop the vehicle
+    //    Output "Go"
+    // Else
+    //    Output "Stop"
+
+    return 0;
+}
