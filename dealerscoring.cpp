41c9a5891ff5ec64925add415da9d7d0e492ebcc
octaviovalentin56-sketch <octaviovalentin56@gmail.com>
2026-05-06 14:15:46 -0400

    Update dealerscoring.cpp

--- dealerscoring.cpp
+++ dealerscoring.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include "dealerscoring.h"
+
+using namespace std;
+
+// To findout the round winner
+void determineWinner{ /
+  int player1Total,
+  int player2Total,
+  int &player1Score,
+  int &player2score
+) {
+
+  cout << "*******************\n";
+  cout << " Round Results\n";
+  cout << "********************\n";
+
+  cout << "Player 1 Results: "
+    << player1Total << endl;
+cout << "Player 2 Results: "
+  << player2Total << endl;
+
+
+  
 
