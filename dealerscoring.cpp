8c17bc0ad2bfd7e02369cbb5e3da9806330641cd
octaviovalentin56-sketch <octaviovalentin56@gmail.com>
2026-05-06 14:29:47 -0400

    Update dealerscoring.cpp

--- dealerscoring.cpp
+++ dealerscoring.cpp
@@ -20,6 +20,16 @@ void determineWinner{ /
 cout << "Player 2 Results: "
   << player2Total << endl;
 
+if ((player1Total == 0 && player2Total == -)
+  || (player1 == player2Total)) {
+cout << "Tie Round \n";
+}
+else if (
+  player1Total == 0
+  || (player2Total > player1Total && player2Total <= 21)
+  ) {
+  cout << "
+
 
   
 
